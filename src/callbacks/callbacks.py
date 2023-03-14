31b22b58f28a205592d31c480db594024850b5c6
pol.gracia.ext <pol.gracia.ext@adevinta.com>
2023-03-14 09:35:45 +0100

    Updated wandb metrics

--- src/callbacks/callbacks.py
+++ src/callbacks/callbacks.py
@@ -4,11 +4,12 @@ import wandb
 
 
 class ImagePredictionLogger(pl.Callback):
-    def __init__(self, val_samples, max_num_samples=32):
+    def __init__(self, val_samples, coder = None, max_num_samples=32):
         super().__init__()
         self.max_num_samples = max_num_samples
         self.val_images = val_samples[0]
         self.val_labels = val_samples[1]
+        self.coder = coder
 
     def on_validation_epoch_end(self, trainer, pl_module):
         if trainer.logger:
@@ -25,8 +26,15 @@ class ImagePredictionLogger(pl.Callback):
                              for x, pred, y in zip(val_images[:self.max_num_samples],
                                                    preds[:self.max_num_samples],
                                                    val_labels[:self.max_num_samples])
-                            ]
-                , "conf_mat" : wandb.plot.confusion_matrix(probs=None,
-                        y_true=val_labels, preds=preds,
-                        class_names=val_labels)
-                })
+                            ],
+                "conf_mat" : wandb.plot.confusion_matrix(probs=None,
+                        y_true=val_labels.numpy(), preds=preds.numpy(),
+                        class_names=list(self.coder.classes_)
+                    ), 
+                "Val confusion Matrix" : wandb.sklearn.plot_confusion_matrix(
+                        y_true = val_labels.numpy(), y_pred = preds.numpy(), labels = list(self.coder.classes_)
+                    ), 
+                "roc" : wandb.plot.roc_curve(
+                        y_true=val_labels.numpy(), y_probas = logits.numpy(), labels = list(self.coder.classes_)
+                    )
+            })
